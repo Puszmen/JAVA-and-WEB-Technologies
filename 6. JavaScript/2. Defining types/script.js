006d3827785e95955caaa113d06e4a802eab05e4
Puszmen <piotr.zatorski91@gmail.com>
2024-02-04 05:49:47 +0100

    added new html and js files and directory, about defining basic types in JS

---
+++ 6. JavaScript/2. Defining types/script.js
@@ -0,0 +1,9 @@
+const days = 35;
+const hours = days * 24;
+const minutes = hours * 60;
+const seconds = minutes * 60;
+
+console.log("Od początku roku minęło " + days + " dni.");
+console.log("To " + hours + " godzin, ");
+console.log(minutes + " minut lub ");
+console.log(seconds + " sekund.");
