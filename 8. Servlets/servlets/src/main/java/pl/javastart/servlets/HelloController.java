28501d64a71953261f0adc9a96c590184114ab51
Puszmen <piotr.zatorski91@gmail.com>
2024-02-19 15:10:59 +0100

    explained in servlet how works constructor and methods init, doGet and destroy

--- 8. Servlets/servlets/src/main/java/pl/javastart/servlets/HelloController.java
+++ 8. Servlets/servlets/src/main/java/pl/javastart/servlets/HelloController.java
@@ -2,13 +2,28 @@ package pl.javastart.servlets;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloController", value = "/hello")
 public class HelloController extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("Hello Servlet!");
+
+    public HelloController() {
+        System.out.println("Konstruktor");
     }
 
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Metoda init");
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("Metoda doGet");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Metoda destroy");
+    }
 }
