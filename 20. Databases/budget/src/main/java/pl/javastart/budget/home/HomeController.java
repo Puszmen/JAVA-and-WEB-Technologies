e6bd44a0d38783f8d3d6da85136f3dbeae6f318e
Puszmen <piotr.zatorski91@gmail.com>
2024-05-20 18:17:52 +0200

    app home budget updated with DTO objects

--- 20. Databases/budget/src/main/java/pl/javastart/budget/home/HomeController.java
+++ 20. Databases/budget/src/main/java/pl/javastart/budget/home/HomeController.java
@@ -1,38 +1,28 @@
-package pl.javastart.homebudget.home;
+package pl.javastart.budget.home;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
-import pl.javastart.budget.BudgetItem;
-import pl.javastart.budget.BudgetItemDao;
-import pl.javastart.budget.BudgetItemType;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import pl.javastart.budget.BudgetItemDto;
+import pl.javastart.budget.BudgetService;
+import pl.javastart.budget.BudgetSummaryDto;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.List;
 
 @WebServlet("")
 public class HomeController extends HttpServlet {
-    private BudgetItemDao budgetItemDao = new BudgetItemDao();
+    private BudgetService budgetService = new BudgetService();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<BudgetItem> incomes = budgetItemDao.findAllItemsByType(BudgetItemType.INCOME);
-        List<BudgetItem> expenses = budgetItemDao.findAllItemsByType(BudgetItemType.EXPENSE);
-        BigDecimal incomesSum = getSum(incomes);
-        BigDecimal expensesSum = getSum(expenses);
-        BigDecimal balance = incomesSum.subtract(expensesSum);
+        List<BudgetItemDto> incomes = budgetService.findAllIncomes();
+        List<BudgetItemDto> expenses = budgetService.findAllExpenses();
+        BudgetSummaryDto summary = budgetService.getSummary();
         request.setAttribute("incomes", incomes);
         request.setAttribute("expenses", expenses);
-        request.setAttribute("incomesSum", incomesSum);
-        request.setAttribute("expensesSum", expensesSum);
-        request.setAttribute("balance", balance);
+        request.setAttribute("summary", summary);
         request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
-    }
-
-    private BigDecimal getSum(List<BudgetItem> items) {
-        return items.stream()
-                .map(BudgetItem::getValue)
-                .reduce(BigDecimal::add)
-                .orElse(BigDecimal.ZERO);
     }
 }
