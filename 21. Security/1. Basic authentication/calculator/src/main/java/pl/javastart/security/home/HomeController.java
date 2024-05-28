1cf625e896fa44bbb2440a8fd2eac7f0d8987a96
Puszmen <piotr.zatorski91@gmail.com>
2024-05-28 21:18:55 +0200

    something about authentication in the code

--- 21. Security/1. Basic authentication/calculator/src/main/java/pl/javastart/security/home/HomeController.java
+++ 21. Security/1. Basic authentication/calculator/src/main/java/pl/javastart/security/home/HomeController.java
@@ -5,11 +5,18 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.security.Principal;
 
 @WebServlet("")
+@ServletSecurity(
+        @HttpConstraint(rolesAllowed = {"user_role", "admin_role"})
+)
 public class HomeController extends HttpServlet {
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Principal userPrincipal = request.getUserPrincipal();
+        System.out.println(userPrincipal.getName());
+        System.out.println(request.isUserInRole("user_role"));
+        System.out.println(request.isUserInRole("admin_role"));
         request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 }
